3382351e39afe770fffabf0e757b53d7102edb43
Leonnardo Basilato <leobasilato@gmail.com>
2023-12-05 14:31:15 -0300

    update

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,42 @@
+import { MagnifyingGlass } from "phosphor-react";
+import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import * as z from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const SearchFormSchema = z.object({
+    query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof SearchFormSchema>
+
+export function SearchForm() {
+
+    const { 
+            register, 
+            handleSubmit,
+            formState: {
+                isSubmitting
+            }
+
+        } = useForm<SearchFormInputs>({
+        resolver: zodResolver(SearchFormSchema),
+    })
+
+
+
+    function handleSearchTransactions(data: SearchFormInputs) {
+        console.log(data)
+    }
+
+    return (
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)} >
+            <input 
+                type="text" 
+                placeholder="Buscar transacoes" 
+                {...register('query')}
+            />
+            <button type="submit" disabled={isSubmitting} > <MagnifyingGlass size={20} />  Buscar</button>
+        </SearchFormContainer>
+    )
+}
